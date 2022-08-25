0873a5d5aa39c3acd30afcb320533ab3ff82f4fe
Captressketh001 <52784916+Captressketh001@users.noreply.github.com>
2022-08-25 04:01:30 +0100

    rounded up

--- src/App.vue
+++ src/App.vue
@@ -4,34 +4,49 @@
   <main>
     <div class="todo-body">
       <div class="todo-header">
-        <h1>TODO</h1>
-        <img src="/images/icon-sun.svg" alt="">
+         <h1>TODO</h1>
+        <!-- <img src="/images/icon-sun.svg" alt="">  -->
+        <div>
+          <label for="toggle"></label>
+         <input type="checkbox" name="" id="slider" @change="toggleTheme">
+        </div>
+        
       </div>
       <div class="todo-input">
+        {{this.$store.getters.alltodos}}
         <span>
           <!-- <img src="/images/icon-check.svg" alt="" style="text-align:center"> -->
         </span>
         <input type="text" placeholder="Create a new todo..." v-model="todoItem" @keyup.enter = "addToDo(todoItem)" required>
       </div>
-      <div class="todo-tasks" v-if="fetchTodo ==''" style="height:80px">
+      <!-- <div class="todo-tasks" v-if="fetchTodo ==''" style="height:80px">
         <p style="text-align:center" >Your Todo List is empty</p>
-      </div>
-      <div class="todo-container" v-else>
-        <div class="todo-tasks" v-for="(todo, ix) in fetchTodo" :key="ix">
+      </div> -->
+
+      <div class="todo-container">
+        <draggable :fetchTodo = fetchTodo>
+        <transition-group name="list">
+        <div class="todo-tasks" v-for="(todo, ix) in fetchTodo" :key="ix" >
           <input type="checkbox" name="" id="" v-model="todo.completed">
           <p v-if="todo.completed" ><s>{{todo.text}}</s></p>
           <p v-else>{{todo.text}}</p>
           <img src="/images/icon-cross.svg" alt="" @click="removeTodo(todo.id)">
         </div>
+        
+          </transition-group>
+        </draggable>
+        <!-- <div class="todo-tasks" @drop="drop(event)" @dragover="allowDrop(event)">
+          
+        </div> -->
          <div class="todo-state">
-          <p>{{this.$store.state.todo.length}} left</p>
-          <div class="active">
-            <p @click="addToFilter('all')">All</p>
-          <p @click="addToFilter(false)">Active</p>
-          <p @click="addToFilter(true)">Completed</p>
+          <p>{{remainder()}} items left</p>
+          <div class="todo-active">
+            <p @click="addToFilter('all')" :class="{active:isActive  =='all'}">All</p>
+            <p @click="addToFilter('active')" :class="{active:isActive  =='active'}">Active</p>
+            <p @click="addToFilter('completed')" :class="{active:isActive  =='completed'}">Completed</p>
           </div>
-          <p @click="clearComplete">Clear Completed</p>
-        </div> 
+           <p @click="clearCompleted">Clear Completed</p>
+         </div> 
       </div>
       <footer>Drag and drop to reorder Tasks</footer>
       
@@ -44,30 +59,40 @@
 <script>
 
 import { v4 } from 'uuid';
+import { VueDraggableNext } from 'vue-draggable-next'
 export default {
   name: 'App',
+  components:{
+    draggable: VueDraggableNext,
+  },
   data (){
     return{
       todoItem:'',
-      filtered:''
+      filtered:'',
+      enabled: true,
+      dragging:false,
+      isActive: 'all'
     }
   },
   computed:{
     fetchTodo(){
       let x  = this.$store.state.todo
-      if (this.filtered == ''){
+      if (this.filtered == '' ){
         return x
       } 
       else{
       return x.filter(list => {
         
-				if (list.completed == this.filtered) {
-					return list.completed == true;
+				if (this.filtered == 'active') {
+          this.isActive = 'active'
+					return !list.completed;
 				}
-				else if (list.active == this.filtered && list.completed == false){
-					return list.active == false;
+				else if (this.filtered == 'completed'){
+          this.isActive = 'completed'
+					return list.completed;
 				}
         else{
+          this.isActive = 'all'
           return x
         }
       
@@ -84,6 +109,8 @@ export default {
                 completed: false,
             }
       this.$store.dispatch('pushTodo', data)
+      this.$store.dispatch('AddTo', this.$store.state.todo)
+      
       this.todoItem = ''
     },
     addToFilter(data){
@@ -97,24 +124,44 @@ export default {
       //   }
       // })
     },
-    clearComplete(){
-      for(let x  in this.$store.state.todo){
-        console.log('x', this.$store.state.todo[x].completed)
-        if(this.$store.state.todo[x].completed){
-          this.$store.state.todo.splice(x);
+    remainder(){
+      return this.$store.getters.remainder
+    },
+    clearCompleted() {
+      Array.prototype.removeWhere = function (callback) {
+        var i = this.length;
+        while (i--) {
+          if (callback(this[i], i)) {
+            this.splice(i, 1);
+          }
         }
-        // console.log('x', this.$store.state.todo[x].completed)
+      };
+     this.$store.state.todo.removeWhere((val) => val.completed);
+    },
+    setTheme(themeName) {
+      localStorage.setItem('theme', themeName);
+      document.documentElement.className = themeName;
+    },
+    toggleTheme() {
+      if (localStorage.getItem('theme') === 'theme-dark') {
+            this.setTheme('theme-light');
+      } 
+      else {
+            this.setTheme('theme-dark');
       }
-      // this.$store.state.todo.filter(i => {
-      //   if(i.completed){
-      //     // this.$store.state.todo.splice(i);
-      //     console.log(typeof this.$store.state.todo)
-      //     console.log('i see')
-      //   
-      // })
-      
-      // console.log(this.$store.state.todo)
+    },
+    setThemeNow () {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        this.setTheme('theme-dark');
+        document.getElementById('slider').checked = false;
+    } else {
+        this.setTheme('theme-dark');
+        document.getElementById('slider').checked = true;
     }
+    }
+  },
+  mounted(){
+      this.setThemeNow()
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -13,7 +13,17 @@ export default createStore({
         },
         deleteTodo(state, id){
             state.todo = state.todo.filter(list => list.id != id)
-        }
+        },
+
+       
+         // let total = 0
+                
+            // getters.cartProducts.forEach(product => {
+                
+            //   total  += product.price * product.quantity
+            // })
+            // return total
+            // return getters.cartProducts.reduce((total, product) => total + product.discounted_price * product.quantity, 0)
 
     },
     actions:{
@@ -26,10 +36,28 @@ export default createStore({
         },
         doDelete(context, id){
             context.commit('deleteTodo', id)
+        },
+        addTo(context, data){
+            localStorage.setItem("todoItem", JSON.stringify(data));
+            context.commit('setTodo', data)
         }
     },
     getters:{
-       allTodos: (state) => state.todo
+        remainder(state){
+            return state.todo.reduce(
+                    (total, todo) => (todo.completed ? total : total + 1),
+                    0
+                  );
+        },
+        alltodos(){
+           return JSON.parse(localStorage.getItem("todoItem"));
+        }
+    //    allTodos: (state) => state.todo,
+//        remainder = () =>
+//   baseTodos.value.reduce(
+//     (total, todo) => (todo.completed ? total : total + 1),
+//     0
+//   );
     }
 
 })
